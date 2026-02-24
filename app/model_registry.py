72c78094a0de18538d1211b46edcbd9fad7f2aed
Lee <contact@lee-scott.com>
2026-02-24 20:35:09 +0000

    Limit registry to LeNet models only

--- app/model_registry.py
+++ app/model_registry.py
@@ -14,9 +14,4 @@ MODEL_REGISTRY: dict[str, ModelSpec] = {
     "best_model_manual_aug_LN": ModelSpec(_MODELS_DIR / "best_model_manual_aug_LN.h5", "LN", "LeNet5, manual warping"),
     "best_model_gs_aug_LN": ModelSpec(_MODELS_DIR / "best_model_gs_aug_LN.h5", "LN", "LeNet5, grid-search warping"),
     "best_model_gan_aug_LN": ModelSpec(_MODELS_DIR / "best_model_gan_aug_LN.h5", "LN", "LeNet5, DCGAN oversampling"),
-
-    "best_model_og_EN": ModelSpec(_MODELS_DIR / "best_model_og_EN.h5", "EN", "EfficientNetV2B0, no augmentation"),
-    "best_model_manual_aug_EN": ModelSpec(_MODELS_DIR / "best_model_manual_aug_EN.h5", "EN", "EfficientNetV2B0, manual warping"),
-    "best_model_gs_aug_EN": ModelSpec(_MODELS_DIR / "best_model_gs_aug_EN.h5", "EN", "EfficientNetV2B0, grid-search warping"),
-    "best_model_gan_aug_EN": ModelSpec(_MODELS_DIR / "best_model_gan_aug_EN.h5", "EN", "EfficientNetV2B0, DCGAN oversampling"),
 }
